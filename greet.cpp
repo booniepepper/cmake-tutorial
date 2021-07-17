52109644d66a473e3b16d26b6da879ce92c61cbb
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-17 00:48:30 -0700

    Say some things about versions

--- greet.cpp
+++ greet.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
+#include <greet_config.h>
+
+int main(int argc, char *argv[]) {
+  // These version variables come from CMake
+  std::cout << argv[0] << " Version " << Greet_VERSION_MAJOR
+            << "." << Greet_VERSION_MINOR << std::endl;
   std::cout << "Hey" << std::endl;
   return 0;
 }
